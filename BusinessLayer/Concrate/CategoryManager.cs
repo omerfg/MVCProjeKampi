9dd8810734a16cba9f2e8d860ab9f750a9e0db50
omerfg <81315022+omerfg@users.noreply.github.com>
2021-04-24 13:28:27 +0300

    MVCProjeKampi

---
+++ BusinessLayer/Concrate/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrate
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repo.List();
+        } 
+
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == ""  || p.CategoryName.Length<=3 || p.CategoryDespcription == "" || p.CategoryName.Length>=51)
+            {
+                //HATA MESAJI
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
